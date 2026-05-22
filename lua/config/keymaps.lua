75e1e4ce0cd5dd173669818c3fb8e20ffb472b3e
jordanyono <jordanyono@gmail.com>
2026-05-22 12:28:40 -0400

    bring snacks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,24 +14,24 @@
   See `:help vim.keymap.set()`.
 ]]
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.thing.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.thing.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.thing.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.thing.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.thing.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.thing.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<leader>th', ':split | terminal<CR>', { desc = 'Terminal horizontal split' })
-vim.keymap.set('n', '<leader>tv', ':vsplit | terminal<CR>', { desc = 'Terminal vertical split' })
-vim.keymap.set('n', '<leader>tt', ':tabnew | terminal<CR>', { desc = 'Terminal in new tab' })
-vim.keymap.set('n', '<leader>tf', ':ToggleTerm<CR>', { desc = 'Toggle floating terminal' })
+vim.thing.set('n', '<leader>th', ':split | terminal<CR>', { desc = 'Terminal horizontal split' })
+vim.thing.set('n', '<leader>tv', ':vsplit | terminal<CR>', { desc = 'Terminal vertical split' })
+vim.thing.set('n', '<leader>tt', ':tabnew | terminal<CR>', { desc = 'Terminal in new tab' })
+vim.thing.set('n', '<leader>tf', ':ToggleTerm<CR>', { desc = 'Toggle floating terminal' })
 
-vim.keymap.set('x', 'p', '"_dP', { noremap = true, silent = true })
+vim.thing.set('x', 'p', '"_dP', { noremap = true, silent = true })
 
 local git_links = require 'config.git_links'
-vim.keymap.set({ 'n', 'v' }, '<leader>go', git_links.open_github, { desc = 'Open in GitHub' })
+vim.thing.set({ 'n', 'v' }, '<leader>go', git_links.open_github, { desc = 'Open in GitHub' })
 
-vim.keymap.set('n', '<leader>x', '<cmd>Neotree toggle<cr>', { desc = 'NeoTree Toggle' })
-vim.keymap.set('n', '<leader>z', '<cmd>Neotree reveal<cr>', { desc = 'NeoTree Reveal' })
+vim.thing.set('n', '<leader>x', '<cmd>Neotree toggle<cr>', { desc = 'NeoTree Toggle' })
+vim.thing.set('n', '<leader>z', '<cmd>Neotree reveal<cr>', { desc = 'NeoTree Reveal' })
