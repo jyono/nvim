8f7d252b4664f5d198081e43ab3a7eb04116bd38
jordanyono <jordanyono@gmail.com>
2026-05-22 13:15:11 -0400

    lazygit

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -61,7 +61,7 @@ return {
         -- for LSP related items. It sets the mode, buffer and description for us each time.
         local map = function(keys, func, desc, mode)
           mode = mode or 'n'
-          vim.thing.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
         -- Rename the variable under your cursor.
--- lua/config/plugins/which_key.lua
+++ lua/config/plugins/which_key.lua
@@ -32,7 +32,7 @@ return {
       { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
       { '<leader>t', group = '[T]oggle' },
       { '<leader>ti', desc = 'LSP inlay hints', mode = { 'n' } },
-      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
+      { '<leader>g', group = 'Git', mode = { 'n', 'v' } },
       { 'gr', group = 'LSP Actions', mode = { 'n' } },
     },
   },
