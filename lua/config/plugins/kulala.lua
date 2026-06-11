97551b9f8efbd5838323e0fb3b26ef44b28cb194
Jordan Yono <jordanyono@gmail.com>
2026-06-11 15:20:18 -0400

    add kulala

--- lua/config/plugins/kulala.lua
+++ lua/config/plugins/kulala.lua
@@ -3,13 +3,13 @@ return {
   {
     'mistweaverco/kulala.nvim',
     keys = {
-      { '<leader>Rs', desc = 'Send request' },
-      { '<leader>Ra', desc = 'Send all requests' },
-      { '<leader>Rb', desc = 'Open scratchpad' },
+      { '<leader>Rs', function() require('kulala').run() end, desc = 'Send request', mode = { 'n', 'v' } },
+      { '<leader>Ra', function() require('kulala').run_all() end, desc = 'Send all requests', mode = { 'n', 'v' } },
+      { '<leader>Rb', function() require('kulala').scratchpad() end, desc = 'Open scratchpad' },
     },
     ft = { 'http', 'rest' },
     opts = {
-      global_keymaps = false,
+      global_keymaps = true,
       global_keymaps_prefix = '<leader>R',
       kulala_keymaps_prefix = '',
     },
--- lua/config/plugins/which_key.lua
+++ lua/config/plugins/which_key.lua
@@ -12,6 +12,7 @@ return {
         { '<leader>t', group = '[T]oggle' },
         { '<leader>td', desc = 'Diagnostic level (all / errors / off)', mode = { 'n' } },
         { '<leader>ti', desc = 'LSP inlay hints', mode = { 'n' } },
+        { '<leader>R', group = 'Kulala / [R]EST' },
         { '<leader>g', group = 'Git', mode = { 'n', 'v' } },
         { 'gr', group = 'LSP Actions', mode = { 'n' } },
       },
